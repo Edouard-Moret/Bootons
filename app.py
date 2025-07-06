fe40884aaf81e012ad742c5cc91b7244c55c89ea
Edouard-Moret <edouard.moret.pro@gmail.com>
2025-07-06 09:04:17 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+import xmlrpc.client
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # appel XML-RPC vers votre instance Odoo
+        client = xmlrpc.client.ServerProxy('https://bootons-test.odoo.com/xmlrpc/2/object')
+        # exécution d’une méthode…
+        result = client.execute_kw('db', 2, 'password', 'res.partner', 'search', [[]])
+        return f"Résultat : {result}"
+    return render_template('index.html')  # bouton dans un petit form
